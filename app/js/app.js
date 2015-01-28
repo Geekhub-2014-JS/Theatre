3ed632d19bf464413eb426f1fbbf3df9b5d129de
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2015-01-28 14:32:03 +0200

    added translating

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,20 @@
 
 /* App Module */
 
-var theatreApp = angular.module('theatre', ['ui.router', 'theatreServices', 'theatreControllers', 'theatreRoutes']);
+var theatreApp = angular.module('theatre', ['ui.router', 'theatreServices', 'theatreControllers', 'theatreRoutes', 'pascalprecht.translate', 'ngCookies']);
+
+theatreApp.config(['$translateProvider', function($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'translating/lang_',
+        suffix: '.json'
+    });
+
+    $translateProvider.preferredLanguage('ua');
+    $translateProvider.useLocalStorage();
+}]);
+
+theatreApp.controller('mainCtrl', ['$scope', '$translate', function($scope, $translate) {
+    $scope.transl = function(lng) {
+        $translate.use(lng);
+    };
+}]);
