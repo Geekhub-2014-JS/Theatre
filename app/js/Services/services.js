03f48654a3c76e7cd2dc893bfc11f5e53bdcf340
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2015-01-29 12:26:49 +0200

    added service for post and get method

--- app/js/Services/services.js
+++ app/js/Services/services.js
@@ -3,3 +3,34 @@
 /* Services */
 
 var theatreServices = angular.module('theatreServices', []);
+
+theatreServices
+    .constant('api_url', 'http://api.theatre.pp.ua/')
+    .factory('apiPost', ['$http', 'api_url',
+        function($http, api_url){
+            return function(url, data) {
+                return $http.post(api_url + url, data)
+                    .success(function(response){
+                        return response.data || [];
+                    })
+                    .error(function(error){
+                        return error;
+                    })
+                    ;
+            }
+        }
+    ])
+    .factory('apiGet', ['$http', 'api_url',
+        function($http, api_url) {
+            return function(url) {
+                return $http.get(api_url + url)
+                    .success(function(response) {
+                        return response.data || [];
+                    })
+                    .error(function(error){
+                        return error;
+                    })
+                    ;
+            }
+        }
+    ]);
