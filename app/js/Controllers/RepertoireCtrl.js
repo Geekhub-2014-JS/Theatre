8d02ffc0871c6bd4fc8c1ef181693cd1022e0714
yanatsulya <yanatsulya@gmail.com>
2015-02-19 23:13:46 +0200

    refactoring

--- app/js/Controllers/RepertoireCtrl.js
+++ app/js/Controllers/RepertoireCtrl.js
@@ -2,7 +2,7 @@ angular.module('repertoire',[])
     .controller('RepertoireCtrl', ['$scope', 'apiGet',
         function($scope, apiGet){
             apiGet('performances.json').success(function (data) {
-                $scope.repertoire = data.performances;
+                $scope.repertoire = data;
             });
         }
     ])
