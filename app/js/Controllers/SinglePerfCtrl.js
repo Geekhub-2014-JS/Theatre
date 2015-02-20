c8f66a82985162e006515d7eb19bd2e237ab77f3
yanatsulya <yanatsulya@gmail.com>
2015-02-20 08:35:42 +0200

    Image carousel on performance page

--- app/js/Controllers/SinglePerfCtrl.js
+++ app/js/Controllers/SinglePerfCtrl.js
@@ -2,8 +2,8 @@
  * Created by Yana on 16.02.2015.
  */
 angular.module('singlePerf',['ui.bootstrap'])
-    .controller('SinglePerformanceCtrl', ['$scope', '$http', 'apiGet', '$stateParams',
-        function ($scope, $http, apiGet, $stateParams) {
+    .controller('SinglePerformanceCtrl', ['$scope', '$q', 'apiGet', '$stateParams',
+        function ($scope, $q, apiGet, $stateParams) {
             apiGet($stateParams.slug + '.json').success(function (data) {
                 $scope.performance = data;
             });
@@ -13,4 +13,7 @@ angular.module('singlePerf',['ui.bootstrap'])
         function ($scope, $window) {
 
         }
-    ]);
+    ])
+    .controller('ImageCarouselCtrl', function ($scope) {
+        $scope.myInterval = 5000;
+    });
