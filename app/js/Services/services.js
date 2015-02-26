1d5f2c06a5b71e12a75b31ffc1d77fd330d338cf
Yuriy Tarnavskiy <etheriq2@gmail.com>
2015-02-27 00:27:11 +0200

    added locale id in service apiGet and apiPost

--- app/js/Services/services.js
+++ app/js/Services/services.js
@@ -6,10 +6,19 @@ var theatreServices = angular.module('theatreServices', []);
 
 theatreServices
     .constant('api_url', '/backend/')
-    .factory('apiPost', ['$http', 'api_url',
-        function ($http, api_url) {
+    .factory('apiPost', ['$http', 'api_url', '$stateParams',
+        function ($http, api_url, $stateParams) {
             return function (url, data) {
-                return $http.post(api_url + url, data)
+                var conf = {
+                    method: 'GET',
+                    url: api_url + url,
+                    headers: {
+                        'locale': $stateParams.locale
+                    },
+                    data: data
+                };
+
+                return $http(conf)
                     .success(function (response) {
                         return response || [];
                     })
@@ -20,10 +29,18 @@ theatreServices
             }
         }
     ])
-    .factory('apiGet', ['$http', 'api_url',
-        function ($http, api_url) {
+    .factory('apiGet', ['$http', 'api_url', '$stateParams',
+        function ($http, api_url, $stateParams) {
             return function (url) {
-                return $http.get(api_url + url)
+                var conf = {
+                    method: 'GET',
+                    url: api_url + url,
+                    headers: {
+                        'locale': $stateParams.locale
+                    }
+                };
+
+                return $http(conf)
                     .success(function (response) {
                         return response || [];
                     })
