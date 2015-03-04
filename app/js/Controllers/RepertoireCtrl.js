e885ce65f7cbb2f529ce5d028535994d04741763
yanatsulya <yanatsulya@gmail.com>
2015-03-04 19:45:53 +0200

    Merge

--- app/js/Controllers/RepertoireCtrl.js
+++ app/js/Controllers/RepertoireCtrl.js
@@ -1,6 +1,26 @@
 angular.module('repertoire',[])
-    .controller('RepertoireCtrl', ['$scope',
-    function($scope){
-        $scope.info = 'Hello from repertoire controller'
-    }
-]);
+    .controller('RepertoireCtrl', ['$scope', 'apiGet',
+        function($scope, apiGet){
+            $scope.itemPerPage = 8;
+            getPerformances(1);
+            $scope.setPage = function (pageNo) {
+                pageNo = $scope.currentPage;
+                getPerformances(pageNo);
+            };
+
+            $scope.maxSize = 5;
+
+            function getPerformances(pageNum) {
+                apiGet('performances' + '?limit=' + $scope.itemPerPage + '&page=' + pageNum)
+                    .success(function (data) {
+                        $scope.repertoire = data.performances;
+                        $scope.totalItems = data.page_count * $scope.itemPerPage; // TODO change after performance_count fix
+                    })
+                    .error(function(error) {
+                        $scope.error = error;
+                    })
+                ;
+            }
+        }
+    ])
+    ;
--- app/js/Services/services.js
+++ app/js/Services/services.js
@@ -5,9 +5,17 @@
 var theatreServices = angular.module('theatreServices', []);
 
 theatreServices
+<<<<<<< Updated upstream
     .constant('api_url', '/backend/')
     .factory('apiPost', ['$http', 'api_url',
         function ($http, api_url) {
+=======
+    .constant('api_url', 'http://api.theatre.pp.ua/')
+    //.constant('api_url', '/backend/')
+
+    .factory('apiPost', ['$http', 'api_url', '$stateParams',
+        function ($http, api_url, $stateParams) {
+>>>>>>> Stashed changes
             return function (url, data) {
                 return $http.post(api_url + url, data)
                     .success(function (response) {
@@ -32,5 +40,27 @@ theatreServices
                     })
                     ;
             }
+<<<<<<< Updated upstream
         }
     ]);
+=======
+        };
+    }])
+
+    .factory('dateConvert', function() {
+        return {
+            perfDate: function(timestamp) {
+                var currentDate = moment(timestamp); // TODO add locale('')
+                var weekDay = ["month.Jan", "month.Feb", "month.Mar", "month.Apr", "month.May", "month.Jun", "month.Jul", "month.Aug", "month.Sept", "month.Oct", "month.Nov", "month.Dec"];
+                var monthN = ["month.Jan", "month.Feb", "month.Mar", "month.Apr", "month.May", "month.Jun", "month.Jul", "month.Aug", "month.Sept", "month.Oct", "month.Nov", "month.Dec"];
+                return {
+                    weekDay: weekDay[currentDate.format("d")],
+                    day: currentDate.format("D"),
+                    month: monthN[currentDate.format("M")+1],
+                    time: currentDate.format("H:mm")
+                };
+            }
+        }
+    })
+;
+>>>>>>> Stashed changes
