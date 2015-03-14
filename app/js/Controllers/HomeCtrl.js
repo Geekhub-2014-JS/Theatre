3b86e336e360e70ccc760fb0d588d7927b2bbe57
yanatsulya <yanatsulya@gmail.com>
2015-03-14 12:56:54 +0200

    Slider added, work in progress

--- app/js/Controllers/HomeCtrl.js
+++ app/js/Controllers/HomeCtrl.js
@@ -1,7 +1,23 @@
-angular.module('home',[])
+angular.module('home',['homepage.bootstrap.carousel'])
     .controller('HomeCtrl', ['$scope',
         function($scope){
             $scope.info = 'Hello from home controller';
+        }
+    ])
+    .controller('HomepageCarouselCtrl', ['$scope', 'apiGet',
+        function($scope, apiGet){
+            apiGet('performanceevents.json?fromDate=today&limit=5')
+                .success(function (data, status) {
+                    if (status === 200) {
+                        $scope.slides = data.performance_events;
+                        $scope.myInterval = 0;
+                    }
+                })
+                .error(function(error) {
+                    $scope.error = error;
+                })
+            ;
+
         }
     ])
 ;
