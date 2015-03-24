f59ec86268cc8f106ef69256d25d37ef401c6f76
vlad-musienko <asmodianko@gmail.com>
2015-03-24 18:35:34 +0200

    Add basic elements on about page

--- app/js/Controllers/AboutCtrl.js
+++ app/js/Controllers/AboutCtrl.js
@@ -1,6 +1,19 @@
-angular.module('about', [])
-    .controller('AboutCtrl', ['$scope',
-        function ($scope) {
-            $scope.info = 'Hello from about controller'
+angular.module('about', ['angular-timeline'])
+    .controller('AboutCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+            $scope.historyStore = [];
+
+            $scope.getHistory = function(year){
+                $http.get("/backend/about/" + year + ".json")
+                    .success(function(data){
+                        $scope.historyStore = data;
+                    })
+                        .error(function(err){
+                        console.log(err);
+                    }
+                )
+            };
+            $scope.getHistory(2010);
         }
-    ]);
+    ])
+;
