811f04219c8b2358f21f4aca325a89d7223384b1
vlad-musienko <asmodianko@gmail.com>
2015-03-24 20:10:07 +0200

    Load theatre history on scrolling page

--- app/js/Controllers/AboutCtrl.js
+++ app/js/Controllers/AboutCtrl.js
@@ -2,18 +2,27 @@ angular.module('about', ['angular-timeline'])
     .controller('AboutCtrl', ['$scope', '$http',
         function ($scope, $http) {
             $scope.historyStore = [];
+            $scope.busy = false;
+            $scope.minDecade = 1980;
+            $scope.currentDecade = 2010;
 
             $scope.getHistory = function(year){
                 $http.get("/backend/about/" + year + ".json")
                     .success(function(data){
-                        $scope.historyStore = data;
+                        $scope.historyStore = $scope.historyStore.concat(data);
                     })
                         .error(function(err){
                         console.log(err);
                     }
                 )
             };
-            $scope.getHistory(2010);
+
+            $scope.loadHistory = function () {
+                if ($scope.busy == true || $scope.currentDecade <= $scope.minDecade) return;
+                $scope.busy = true;
+                $scope.getHistory($scope.currentDecade);
+                $scope.currentDecade -= 10;
+                $scope.busy = false;
+            };
         }
-    ])
-;
+    ]);
