81ded98f7fbce21fe27766eab2aa602b1961d4fb
Kovaluov Dmitriy <kds16061985@gmail.com>
2017-03-01 09:15:06 +0200

    hall directive changes

--- src/js/Directives/HallDirective.js
+++ src/js/Directives/HallDirective.js
@@ -1,14 +1,27 @@
 angular.module('hallDirectives', [])
-    .directive('hallView', function (hallService) {
+    .directive('hallView', function () {
         return {
             restrict: 'E',
             template: "<div ng-include='getUrl()'></div>",
-            controller:'hallCtrl',
+            controller: 'hallCtrl',
 
-            link:function (scope,elem,attrs) {
-                elem[0].addEventListener("click", function(event){
-                    if (event.target.tagName==='LI')
-                        scope.addToCart(event.target.getAttribute('data-sits'))
+            link: function (scope, elem, attrs) {
+                elem[0].addEventListener("click", function (event) {
+                    if (event.target.tagName === 'LI') {
+                        var nodeParent = event.target;
+                        var sitsData={};
+                        sitsData.sit=event.target.getAttribute('data-place');
+
+                        while (!sitsData.row&&!sitsData.sits) {
+                            nodeParent = nodeParent.parentNode;
+                            if (nodeParent.hasAttribute('data-row')) sitsData.row = nodeParent.getAttribute('data-row');
+                            if (nodeParent.hasAttribute('data-section')) sitsData.section = nodeParent.getAttribute('data-section');
+                        }
+                        scope.addToCart(sitsData)
+                    }
+
+
+
                 });
             }
 
